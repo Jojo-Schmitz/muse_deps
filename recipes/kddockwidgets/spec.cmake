3dfef04ee656043e27144a9c1543124b84a655bf
Eism <eismailzada@gmail.com>
2026-06-25 14:47:44 +0300

    link kddockwidgets to the last 2.4 commit

--- recipes/kddockwidgets/spec.cmake
+++ recipes/kddockwidgets/spec.cmake
@@ -1,5 +1,5 @@
 set(DEP_VERSION 2.4)
 
 set(DEP_SOURCES
-    "kddockwidgets|tarball|https://github.com/musescore/KDDockWidgets/archive/aca473a2362103d93c70c3fe153ac9a48f09b1b3.tar.gz|88a7d1848469a59887528e09c24b52020139003b04a6c19db6bac45ea9fc31b2"
+    "kddockwidgets|tarball|https://github.com/musescore/KDDockWidgets/archive/5937b539edb12940d70e746c35f7887592b6afe6.tar.gz|df53c4d4c856b3741b5cc30e0942843568b8b1a662f8ed1d04c694564b0e1386"
 )
