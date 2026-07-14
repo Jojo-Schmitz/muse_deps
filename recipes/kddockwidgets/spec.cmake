f297717d1f3819e5ac16d2053db447bb54cf9018
Eism <eismailzada@gmail.com>
2026-07-14 12:34:42 +0300

    updated kddockwidgets

--- recipes/kddockwidgets/spec.cmake
+++ recipes/kddockwidgets/spec.cmake
@@ -1,5 +1,5 @@
 set(DEP_VERSION 2.4)
 
 set(DEP_SOURCES
-    "kddockwidgets|tarball|https://github.com/musescore/KDDockWidgets/archive/5937b539edb12940d70e746c35f7887592b6afe6.tar.gz|ff924045369e0b4ae134f34bbb2c777e2d91220cb35eaa451add03cf6453cfb5"
+    "kddockwidgets|tarball|https://github.com/musescore/KDDockWidgets/archive/27aa5391772aa5b8100af5f59adb7ed5d123f6ba.tar.gz|4f785d166f763b36484da086c428ec8199bd49d57110e2cc74be8d2ecb8fdc76"
 )
